c524941610c946a29cdf83f784932f1c6b594fc2
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-12-12 21:00:12 -0300

    refactor: [add] versão inicial

--- src/dsgovbr/apps.py
+++ src/dsgovbr/apps.py
@@ -3,7 +3,6 @@ from django.apps import AppConfig
 
 
 class DSGovBRConfig(AppConfig):
-    name: str = "DSGovBR"
-    verbose_name: str = "Outono de 2023"
+    name: str = "dsgovbr"
+    verbose_name: str = "DSGovBR"
     icon: str = "fa fa-edit"
-    is_painel_theme: bool = True
