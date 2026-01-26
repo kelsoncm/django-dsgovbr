9f438fd3b31775619d146a5994565b8ef3599403
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-01-26 18:27:19 -0300

    feat: [add] lê a disponibiliza as settings para o tema

--- src/dsgovbr/context_processors.py
+++ src/dsgovbr/context_processors.py
@@ -5,9 +5,15 @@ from django.http import HttpRequest
 
 def layout_settings(request: HttpRequest) -> dict:
     return {
-        "app_label": getattr(settings, "APP_LABEL", "APP_LABEL"),
-        "app_version": getattr(settings, "APP_VERSION", "APP_VERSION"),
-        "app_last_startup": getattr(settings, "APP_LAST_STARTUP", "APP_LAST_STARTUP"),
+        "project_company": getattr(settings, "PROJECT_COMPANY", "PROJECT_COMPANY"),
+        "project_title": getattr(settings, "PROJECT_TITLE", "PROJECT_TITLE"),
+        "project_subtitle": getattr(settings, "PROJECT_SUBTITLE", "PROJECT_SUBTITLE"),
+        "project_version": getattr(settings, "PROJECT_VERSION", "PROJECT_VERSION"),
+        "project_last_startup": getattr(settings, "PROJECT_LAST_STARTUP", "PROJECT_LAST_STARTUP"),
+        "project_copyright": getattr(settings, "PROJECT_COPYRIGHT", "@2025 PROJECT_COPYRIGHT"),
+        "project_license": getattr(settings, "PROJECT_LICENSE", "Licença MIT"),
+        "project_license_url": getattr(settings, "PROJECT_LICENSE_URL", "https://opensource.org/license/mit"),
+
         "hostname": getattr(settings, "HOSTNAME", "HOSTNAME"),
         "user_avatar": "https://cdn-icons-png.freepik.com/512/6596/6596121.png",
         "have_header_menu_trigger": True,
