fdc71a3a494405e0fba9ad46bdc7d3019a9577ab
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-01-27 10:49:30 -0300

    feat: [add] numera a versão para indicar a versão do Django que é suportada

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-dsgovbr',
-    version='0.1.1',
+    version='5.2.2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
