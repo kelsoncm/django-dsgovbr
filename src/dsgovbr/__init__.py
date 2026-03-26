8a8ba1355e6276c0a689cf72ef3f12ff1eae7ecc
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-03-26 20:33:06 -0300

    Refactor Django admin templates and styles for improved UI/UX
    - Updated `change_form.html` and `change_form_object_tools.html` to enhance object tools layout.
    - Added new CSS styles in `dsgovbr.css` for better table and button designs.
    - Introduced pagination and search form templates for a more responsive design.
    - Created new filter and action templates to streamline admin actions.
    - Implemented new widget templates for improved form handling.
    - Added support for import/export functionality in admin views.
    - Enhanced object history and login templates for better user experience.
    - Refactored existing templates to use new block structures for consistency.

---
+++ src/dsgovbr/__init__.py
@@ -0,0 +1,1 @@
+from django.contrib.admin.templatetags import admin_list
--- src/dsgovbr/templatetags/dsgovbr.py
+++ src/dsgovbr/templatetags/dsgovbr.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils.safestring import SafeString, mark_safe
+from django.contrib.admin.views.main import PAGE_VAR
+
 register = template.Library()
 
 
@@ -10,3 +13,20 @@ def first_word(value):
 @register.filter
 def user_letters(user):
     return user.first_name[:1].upper() + user.last_name[:1].upper()
+
+
+@register.simple_tag
+def page_item(cl, page) -> SafeString:
+    if page == cl.paginator.ELLIPSIS:
+        extra_class = "disabled"
+    elif page == cl.page_num:
+        extra_class = "active"
+    else:
+        extra_class = ""
+
+    url = cl.get_query_string({PAGE_VAR: page})
+
+    return mark_safe(
+        f"""<li class="paginate_button page-item {extra_class}"><a href="{url}"
+            data-dt-idx="{page}" tabindex="{page}" class="page-link">{page}</a></li>"""
+    )
