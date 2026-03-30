531fb4083e3fb75e3784692122e39c605cfb0cdb
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-03-30 20:02:57 -0300

    refactor(admin): simplificar importações e remover código comentado no dsgovbr_admin_list.py

--- src/dsgovbr/templatetags/dsgovbr_admin_list.py
+++ src/dsgovbr/templatetags/dsgovbr_admin_list.py
@@ -1,39 +1,21 @@
+from django.utils.translation import gettext as _
 import datetime
-
 from django.contrib.admin.templatetags.admin_urls import add_preserved_filters
-from django.contrib.admin.utils import (
-    display_for_field,
-    display_for_value,
-    get_fields_from_path,
-    label_for_field,
-    lookup_field,
-)
-from django.contrib.admin.views.main import (
-    ALL_VAR,
-    IS_FACETS_VAR,
-    IS_POPUP_VAR,
-    ORDER_VAR,
-    PAGE_VAR,
-    SEARCH_VAR,
-)
+from django.contrib.admin.utils import display_for_field, display_for_value, label_for_field, lookup_field
+from django.contrib.admin.views.main import ORDER_VAR
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models.constants import LOOKUP_SEP
 from django.template import Library
 from django.template.loader import get_template
-from django.templatetags.static import static
 from django.urls import NoReverseMatch
-from django.utils import formats, timezone
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-from django.utils.text import capfirst
-from django.utils.translation import gettext as _
-
 from django.contrib.admin.templatetags.base import InclusionAdminNode
 
 
-
 register = Library()
+
 
 # Filter para permitir spec|choices:cl no template
 @register.filter
@@ -57,54 +39,6 @@ def first_query_param(query_string):
             k, v = pair.split('=', 1)
             return {'name': k, 'value': v}
     return {}
-
-
-# @register.simple_tag
-# def paginator_number(cl, i):
-#     """
-#     Generate an individual page index link in a paginated list.
-#     """
-#     if i == cl.paginator.ELLIPSIS:
-#         return format_html("{} ", cl.paginator.ELLIPSIS)
-#     elif i == cl.page_num:
-#         return format_html('<span class="this-page">{}</span> ', i)
-#     else:
-#         return format_html(
-#             '<a href="{}"{}>{}</a> ',
-#             cl.get_query_string({PAGE_VAR: i}),
-#             mark_safe(' class="end"' if i == cl.paginator.num_pages else ""),
-#             i,
-#         )
-
-
-# def pagination(cl):
-#     """
-#     Generate the series of links to the pages in a paginated list.
-#     """
-#     pagination_required = (not cl.show_all or not cl.can_show_all) and cl.multi_page
-#     page_range = (
-#         cl.paginator.get_elided_page_range(cl.page_num) if pagination_required else []
-#     )
-#     need_show_all_link = cl.can_show_all and not cl.show_all and cl.multi_page
-#     return {
-#         "cl": cl,
-#         "pagination_required": pagination_required,
-#         "show_all_url": need_show_all_link and cl.get_query_string({ALL_VAR: ""}),
-#         "page_range": page_range,
-#         "ALL_VAR": ALL_VAR,
-#         "1": 1,
-#     }
-
-
-# @register.tag(name="pagination")
-# def pagination_tag(parser, token):
-#     return InclusionAdminNode(
-#         parser,
-#         token,
-#         func=pagination,
-#         template_name="pagination.html",
-#         takes_context=False,
-#     )
 
 
 def _coerce_field_name(field_name, field_index):
